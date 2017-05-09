9ee32b25f7280d43eee4c8606dee0b6da879bd03
Moyun <moyunmo@hotmail.com>
2017-05-09 14:01:47 +0800

    modify the podspec file

---
+++ Example/Vuforia/CMTest.mm
@@ -0,0 +1,14 @@
+//
+//  CMTest.m
+//  Vuforia
+//
+//  Created by Moyun on 2017/5/9.
+//  Copyright © 2017年 momo605654602@gmail.com. All rights reserved.
+//
+
+#import "CMTest.h"
+#import <System.h>
+
+@implementation CMTest
+
+@end
